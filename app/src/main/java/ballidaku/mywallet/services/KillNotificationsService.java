dbb7957b22a59befece088538434d316f5dbcdbf
sharanpal singh <sharanpal.singh@brihaspatitech.com>
2018-09-20 15:23:17 +0530

    Updated on 20/9/2018 03:46 PM

--- app/src/main/java/ballidaku/mywallet/services/KillNotificationsService.java
+++ app/src/main/java/ballidaku/mywallet/services/KillNotificationsService.java
@@ -1,15 +1,6 @@
-package ballidaku.mywallet.services
+package ballidaku.mywallet.services;
 
-import android.app.NotificationManager
-import android.app.Service
-import android.content.Context
-import android.content.Intent
-import android.os.Binder
-import android.os.IBinder
-
-import ballidaku.mywallet.commonClasses.MyConstant
-
-class KillNotificationsService : Service()
+/*class KillNotificationsService : Service()
 {
 
     private var mNM: NotificationManager? = null
@@ -33,22 +24,31 @@ class KillNotificationsService : Service()
         assert(mNM != null)
         mNM!!.cancel(MyConstant.NOTIFICATION_ID)
     }
-}
+}*/
 
 
-/*public class KillNotificationsService extends Service
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import ballidaku.mywallet.commonClasses.MyConstant;
+
+public class KillNotificationsService extends Service
 {
+
+    private NotificationManager mNM;
+    private final IBinder mBinder = new KillBinder(this);
+
     public class KillBinder extends Binder
     {
         public final Service service;
-        public KillBinder(Service service)
+        KillBinder(Service service)
         {
             this.service = service;
         }
     }
-
-    private NotificationManager mNM;
-    private final IBinder mBinder = new KillBinder(this);
 
     @Override
     public IBinder onBind(Intent intent)
@@ -69,4 +69,4 @@ class KillNotificationsService : Service()
         assert mNM != null;
         mNM.cancel(MyConstant.NOTIFICATION_ID);
     }
-}*/
+}
